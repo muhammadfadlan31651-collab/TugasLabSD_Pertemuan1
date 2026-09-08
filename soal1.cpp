86b084ba05a04f758b7136bb185f18085b20727e
muhammadfadlan31651-collab <muhammad.fadlan31651@sma.belajar.id>
2026-09-08 19:31:45 +0700

    Update soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,28 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int data[3][3][4];
+    int angka = 2;
+ 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                data[i][j][k] = angka;
+                angka += 2;
+            }
+        }
+    }
+ 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                cout << data[i][j][k] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+ 
+    return 0;
+}
